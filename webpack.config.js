a86442ac021eb49504280b072d9bfa6345d66ee3
eatWhatLunch <9706755@qq.com>
2018-04-21 01:30:04 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -15,17 +15,28 @@ const defaultPath = {
 
 module.exports = {
     entry:{
-        vendor: ["rxjs"],
-        app: "./src/app.js"
+        app: "./src/app.js",
+        vendor: ["./src/components/bpp.js"]//需要作为vendor块打包的文件
     },
+    target:"web",//告诉webpack当前运行的环境，"node"代表nodejs环境,如文件采用require方式引用
     output: {
         filename: env==="production"?"js/[name]_[hash:8].js":"app.js",
         // filename: "app.js",
         path:path.resolve(__dirname,"./dist/assets"),//打包及发布出来的文件路径
         publicPath: "/assets/",//打包后的资源的引用路径=publicPath+资源名
-        chunkFilename:'js/[name]_[chunkhash:8].chunk.js'//用于打包按需加载的文件
+        chunkFilename:'js/[name]_[chunkhash:8].chunk.js',//用于打包按需加载的文件
+        libraryTarget: 'var'//告诉webpack使用什么样的规范,常用的有
+                                  //1."commonjs2":用于node环境,即打包后的文件内会以module.exports={}的形式来组织代码
+                                  //2."umd"用umd的模块化写法
     },
     module: {
+        noParse: (content) => {
+            /**
+             * 不会解析满足条件的文件中的依赖关系
+             * 因此noParse的文件中不应有import,require,define的调用
+             */
+            return /rxjs/.test(content);
+        },
         rules: [
             {
                 test: /\.js$/,
@@ -38,7 +49,8 @@ module.exports = {
                 test: /\.css$/,
                 use: [
                     env==="production"?MiniCssExtractPlugin.loader:"style-loader",
-                    "css-loader"
+                    "css-loader",
+                    "postcss-loader"
                 ]
             },
             {
@@ -102,8 +114,7 @@ module.exports = {
         }),
         new webpack.ProvidePlugin({
             /**
-             * 自动加载模块,这样在文件中就不需要再用import或require来引入模块,
-             * webpack会自动找到对应模块
+             * 自动加载模块,这样在文件中就不需要再用import或require来引入模块,webpack会自动找到对应模块
              */
             $: "jquery",
             jQuery: "jquery",
@@ -120,13 +131,16 @@ module.exports = {
         new MiniCssExtractPlugin({
             // Options similar to the same options in webpackOptions.output
             // both options are optional
-            filename: env==="production"?"[name]_[hash:8].css":"[name].css",
-            chunkFilename: env==="production"?"[id]_[hash:8].css":"[id].css"
+            filename: env==="production"?"css/[name]_[hash:8].css":"[name].css",
+            chunkFilename: env==="production"?"css/[id]_[hash:8].css":"[id].css"
           })
     ],
     optimization: {
         splitChunks: {
-            chunks: "async",//"initial"=>打包初始的块;"async"=>打包按需加载的块;"all"=>打包所有快
+            /**
+             * 配置需要单独打包的块
+             */
+            chunks: "async",//"initial"=>打包初始块;"async"=>打包按需加载的块;"all"=>打包所有块
 
             //webpack默认根据以下四个条件进行打包
             minSize: 30000,//每个块的最小大小
@@ -154,7 +168,7 @@ module.exports = {
         runtimeChunk: true,//设置为true时，会给每一个入口文件都生成一个额外的只包含运行时代码的文件
         minimizer: [
             /**
-             * webpack4 production下默认会压缩JS
+             * webpack4 production模式下默认会压缩JS
              * 以下为自定义配置
              */
             new UglifyJsPlugin({
@@ -166,9 +180,13 @@ module.exports = {
                 sourceMap: true,//是否生成SourceMap
             }),
             new OptimizeCSSAssetsPlugin({
+                /**
+                 * 用于压缩CSS
+                 */
                 cssProcessor: require('cssnano'),//用于优化|压缩CSS的处理器,默认使用cssnano
                 cssProcessorOptions: { 
-                    //具体可配置内容见http://cssnano.co/optimisations/
+                    //具体可配置内容见
+                    //http://cssnano.co/optimisations/
                     discardComments: { 
                         removeAll: true//移除所有注释
                     },
@@ -185,16 +203,30 @@ module.exports = {
         ]
     },
     resolve: {
+        alias: {
+            /**
+             * 用key作为value的别名,在import/require时会更快
+             * 例：原本需要import app from "../../x/y/src/app"
+             *     现在import app from "src/app"
+             */
+            src: defaultPath.src
+        },
         modules: [
             /**
-             * webpack解析模块时去搜索的目录
-             * 例：在defaultPath内定义好文件的路径,当你在任何文件内需要加载src目录内的文件时,可以直接使用src目录,如:const src = require("src/xxx"),webpack会按照从上到下的顺序去寻找文件
+             * webpack解析模块时去搜索的目录,会按照从上到下的顺序去寻找文件
              */
             "node_modules",
             defaultPath.src
-        ]
+        ],
+        extensions: [".js",".tsx"]//导入模块时可以忽略后缀名
     },
     devServer: {
         historyApiFallback: true
+    },
+    externals: {
+        /**
+         * 如果希望从如cdn等地方引入外部模块,可以设置external来使webpack不打包指定内容
+         */
+        // jquery: "jQuery"//意思是:当require("jquery")时，你需要将jQuery暴露出来作为全局变量来使用
     }
 }
